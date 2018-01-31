c64a8f641ffe946dd5f607676924185df4068505
mrropain <mrropain@gmail.com>
2018-01-31 09:28:57 +0100

    implement first mina package

---
+++ src/me/ropain/mina/protect/MinaProtect.java
@@ -0,0 +1,21 @@
+package me.ropain.mina.protect;
+
+import me.ropain.mina.core.packages.IPackage;
+
+public class MinaProtect implements IPackage {
+
+    @Override
+    public void load() {
+
+    }
+
+    @Override
+    public void unload() {
+
+    }
+
+    @Override
+    public Object[] listeners() {
+        return new Object[0];
+    }
+}
