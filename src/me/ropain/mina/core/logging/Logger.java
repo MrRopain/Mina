f86fc4a7ea6b663e616bf9ade16b96830e2612bd
mrropain <mrropain@gmail.com>
2018-01-31 06:12:01 +0100

    simple logging

---
+++ src/me/ropain/mina/core/logging/Logger.java
@@ -0,0 +1,41 @@
+package me.ropain.mina.core.logging;
+
+import me.ropain.mina.core.Mina;
+import me.ropain.mina.core.l10n.L10n;
+import me.ropain.mina.core.l10n.Localizable;
+import me.ropain.mina.core.l10n.LocalizableValues;
+
+/**
+ * Allows logging on different levels with automated localization.
+ *
+ * @author MrRopain
+ */
+public class Logger {
+
+    /**
+     * Logs a localized string with inserted values on the given logging level.
+     */
+    public static void log(LoggingLevel level, Localizable localizable, LocalizableValues values) {
+        org.slf4j.Logger logger = Mina.getLogger();
+        String localizedString = L10n.localize(localizable, values);
+
+        switch (level) {
+            case INFO:
+                logger.info(localizedString);
+                break;
+            case WARN:
+                logger.warn(localizedString);
+                break;
+            case ERROR:
+                logger.error(localizedString);
+                break;
+        }
+    }
+
+    /**
+     * Logs a localized string on the given logging level.
+     */
+    public static void log(LoggingLevel level, Localizable localizable) {
+        log(level, localizable, LocalizableValues.build());
+    }
+}
---
+++ src/me/ropain/mina/core/logging/LoggingLevel.java
@@ -0,0 +1,12 @@
+package me.ropain.mina.core.logging;
+
+/**
+ * Contains logging levels.
+ *
+ * @author MrRopain
+ */
+public enum LoggingLevel {
+    INFO,
+    WARN,
+    ERROR
+}
