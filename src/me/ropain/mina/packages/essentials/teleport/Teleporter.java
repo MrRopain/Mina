3f4567af81679942cca1fca29ceb2c7d5f7f4323
mrropain <mrropain@gmail.com>
2018-02-01 17:52:16 +0100

    remove responses - they belong in the respective command executors

--- src/me/ropain/mina/packages/essentials/teleport/Teleporter.java
+++ src/me/ropain/mina/packages/essentials/teleport/Teleporter.java
@@ -1,21 +1,14 @@
 package me.ropain.mina.packages.essentials.teleport;
 
 
-import me.ropain.mina.core.config.Config;
-import me.ropain.mina.core.l10n.L10n;
-import me.ropain.mina.core.l10n.LocalizableValues;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 import org.spongepowered.api.event.filter.Getter;
-import org.spongepowered.api.scheduler.Task;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.chat.ChatTypes;
 import org.spongepowered.api.world.Location;
 import org.spongepowered.api.world.World;
 
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Contains functions to easily teleport players.
@@ -41,26 +34,26 @@ public class Teleporter {
      * Teleports an player to another player.
      */
     public static void teleport(Player player, Player target) {
-        String message = L10n.localize("teleport/message", LocalizableValues.build("player", target.getName()));
-
         updateLastLocation(player);
         player.setLocation(target.getLocation());
-        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(message));
+        player.setVehicle(target);
+        target.addPassenger(player);
     }
 
     /**
      * Teleports the player to the given location.
      */
     public static void teleport(Player player, Location<World> location) {
-        String message = L10n.localize("teleport_location/message", LocalizableValues.build()
-                .add("x", location.getBlockX())
-                .add("y", location.getBlockY())
-                .add("z", location.getBlockZ())
-        );
-
         updateLastLocation(player);
         player.setLocation(location);
-        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(message));
+    }
+
+    /**
+     * Transfers the player to the given world.
+     */
+    public static void teleport(Player player, World world) {
+        updateLastLocation(player);
+        player.transferToWorld(world);
     }
 
     /**
@@ -73,7 +66,6 @@ public class Teleporter {
         }
 
         player.setLocation(lastLocations.get(player));
-        player.sendMessage(ChatTypes.ACTION_BAR, Text.of(L10n.localize("back/message")));
     }
 
     /**
@@ -88,6 +80,9 @@ public class Teleporter {
         lastLocations.put(player, player.getLocation());
     }
 
+    /**
+     * Returns the instance.
+     */
     public static Teleporter getInstance() {
         if (instance == null) {
             instance = new Teleporter();
