17ffb3390b9f6526ba15a5a40c5522554aad48b5
mrropain <mrropain@gmail.com>
2018-02-02 01:41:33 +0100

    forgot this, oops

--- src/me/ropain/mina/packages/protect/MinaProtect.java
+++ src/me/ropain/mina/packages/protect/MinaProtect.java
@@ -1,5 +1,6 @@
 package me.ropain.mina.packages.protect;
 
+import me.ropain.mina.core.commands.AbstractCommand;
 import me.ropain.mina.core.packages.IPackage;
 
 public class MinaProtect implements IPackage {
@@ -17,5 +18,10 @@ public class MinaProtect implements IPackage {
     @Override
     public Object[] getListeners() {
         return new Object[0];
+    }
+
+    @Override
+    public AbstractCommand[] getCommands() {
+        return new AbstractCommand[0];
     }
 }
